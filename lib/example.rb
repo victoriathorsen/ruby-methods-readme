df664cefc288d62ae75ce2cc124f20c022fce028
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-07 16:09:23 -0800

    <added code for assignement, greeting hello world>

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,29 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+  end 
+
+  def greeting # Method Signature
+    puts "Hello World" # Method Body
+  end # Method Closing 
+
+  def greeting
+    # Leave a line break for the method body
+  end # Immediately close the method.  
+
+  def greeting
+    puts "Hello World" # Now code the body of the method.
+  end 
